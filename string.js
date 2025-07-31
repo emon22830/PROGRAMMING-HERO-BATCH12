1d8d78f073302fd13b14d763747df8d9dcb82bb5
emon22830 <mdemon22830@gmail.com>
2025-07-31 22:31:17 +0800

    string

---
+++ string.js
@@ -0,0 +1,15 @@
+const country = 'Bangladesh';
+const division = 'Dhaka';
+const district = "Munshiganj"
+const village = `Aralia`
+// const thana = new String('Demra');
+const numbers = [54,56,5766,565,45]
+console.log(typeof thana);
+console.log(numbers.length);
+console.log(numbers[1])
+const capital = 'Dhaka ';
+console.log(capital[4])
+numbers[1] = 2
+console.log(numbers)
+capital[1] ='a';
+console.log(capital)
