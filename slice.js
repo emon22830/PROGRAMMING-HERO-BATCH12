274b832a8860f3d0074fd70a5870a48878efd614
emon22830 <mdemon22830@gmail.com>
2025-08-01 20:15:36 +0800

    string explore

---
+++ slice.js
@@ -0,0 +1,7 @@
+const address = 'Aralia';
+const part = address.slice(2,6);
+console.log(part);
+
+const sentence = 'I am a goood hardworking person';
+// console.log(sentance.split(' '))
+console.log(sentence.split('a')) 
